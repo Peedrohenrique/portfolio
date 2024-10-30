91966b31f864cb1d44298fca3cf75306687ebf97
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2024-10-30 12:03:55 -0300

    finished header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,40 @@
 "use client"
+import BlurFade from "@/components/ui/blur-fade";
+import BlurFadeText from "@/components/ui/blur-fade-text";
+import { DATA } from "@/data/resume";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import * as React from "react"
+
+const BLUR_FADE_DELAY = 0.04;
 export default function Home() {
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)] gap-3">
-      <h1 className="text-6xl font-bold text-center sm:text-left">Pedro Portfólio</h1>
-
-      <div className="flex gap-2">
-        <span className="h-1  border-b border-gray-300"></span>
-        <p className="text-sm text-center text-gray-500 border-b border-gray-500">Em breve</p>
-      </div>
-    </div>
+    <main className="flex flex-col min-h-[100dvh] space-y-10">
+      <section id="hero">
+        <div className="mx-auto w-full max-w-2xl space-y-8">
+          <div className="flex justify-between gap-2">
+            <div className="flex-col flex flex-1 space-y-1.5">
+              <BlurFadeText
+                delay={BLUR_FADE_DELAY}
+                className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+                yOffset={8}
+                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
+              />
+              <BlurFadeText
+                className="max-w-[600px] md:text-xl"
+                delay={BLUR_FADE_DELAY}
+                text={DATA.description}
+              />
+            </div>
+            <BlurFade delay={BLUR_FADE_DELAY}>
+              <Avatar className="size-28 border">
+                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+                <AvatarFallback>{DATA.initials}</AvatarFallback>
+              </Avatar>
+            </BlurFade>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
